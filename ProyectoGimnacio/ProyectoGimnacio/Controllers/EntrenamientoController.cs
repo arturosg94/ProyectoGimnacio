248faed158364fd99034ea75a1515aef0d29a089
arturosg94 <arturo.sandoval.gonzalez.94@gmail.com>
2019-11-06 22:21:46 -0500

    Creacion de Clases

--- ProyectoGimnacio/ProyectoGimnacio/Controllers/EntrenamientoController.cs
+++ ProyectoGimnacio/ProyectoGimnacio/Controllers/EntrenamientoController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProyectoGimnacio.Models;
+using ProyectoGimnacio.Clases;
 
 namespace ProyectoGimnacio.Controllers
 {
@@ -55,6 +57,26 @@ namespace ProyectoGimnacio.Controllers
             {
                 return RedirectToAction("Login", "Home");
             }
+        }
+
+        public JsonResult MostrarEjercicio()
+        {
+            JsonResult response = null;
+            List<Ejercicio> list = new List<Ejercicio>();
+            EntrenamientoModel cn = new EntrenamientoModel();
+            list = cn.MostrarEjercicio();
+            response = Json(list, JsonRequestBehavior.AllowGet);
+            return response;
+        }
+
+        public JsonResult AgregarEjercicio(string Nombre,string Imagen)
+        {
+            JsonResult response = null;
+            List<E_MensajeSalida> list = new List<E_MensajeSalida>();
+            EntrenamientoModel cn = new EntrenamientoModel();
+            list = cn.AgregarEjercicio(Nombre, Imagen);
+            response = Json(list, JsonRequestBehavior.AllowGet);
+            return response;
         }
     }
 }
